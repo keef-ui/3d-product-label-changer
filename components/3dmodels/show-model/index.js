0cffea97c6b3f50718c75b27c97507938e66c17d
ui-coder-tv <ui-coder-tv@googlemail.com>
2022-06-28 22:38:24 +0100

    takeSnaphot function  - Created HTML element for button and click handler

--- components/3dmodels/show-model/index.js
+++ components/3dmodels/show-model/index.js
@@ -1,17 +1,55 @@
 import { useRef,useState } from "react"
-import {Canvas} from "@react-three/fiber"
-import { useGLTF, PerspectiveCamera, OrbitControls } from '@react-three/drei'
+import {Canvas, useThree} from "@react-three/fiber"
+import { useGLTF, PerspectiveCamera, OrbitControls, Html } from '@react-three/drei'
 
 // ShowModel properties:
 //----------------------
 // camPosition:   is array value for the  position of perspective camera 
 // children : The pass through child components
 
+//Resources: 
+// Snapshot: https://codesandbox.io/s/basic-demo-forked-rnuve?file=/src/App.js:1540-1580
+
+function TestButton() {
+  const gl = useThree((state) => state.gl);
+  const takeSnapShot = () => {const link = document.createElement("a");
+                link.setAttribute("download", "canvas.png");
+                link.setAttribute(
+                  "href",
+                  gl.domElement
+                    .toDataURL("image/png")
+                    .replace("image/png", "image/octet-stream")
+                );
+                link.click();
+  }
+
+  return (
+    <Html className="content" position={[0, 50, 20]}>
+      <button onClick={takeSnapShot} type="button">
+        Button...
+      </button>
+    </Html>
+  );
+}
+
 export default function ShowModel({children,...props}) {
 //  props.camPosition[0]=props.cx;
+        //  const gl = useThree((state) => state.gl);
+          // const takeSnapShot = () => {const link = document.createElement("a");
+          //               link.setAttribute("download", "canvas.png");
+          //               link.setAttribute(
+          //                 "href",
+          //                 gl.domElement
+          //                   .toDataURL("image/png")
+          //                   .replace("image/png", "image/octet-stream")
+          //               );
+          //               link.click();
+          // }
+          
+
     return (
       <>
-        <Canvas>
+        <Canvas gl={{ preserveDrawingBuffer: true }}>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
           {children}
@@ -20,6 +58,8 @@ export default function ShowModel({children,...props}) {
             position={[props.camPosX, props.camPosY, props.camPosZ]}
           />
           <OrbitControls />
+          <TestButton />
+
         </Canvas>
       </>
     );
