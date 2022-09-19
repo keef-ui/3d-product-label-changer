adba8dc016f2ecbaa6f55df21614481e868a41f9
Keerthi <keef>
2022-09-19 17:08:32 +0100

    Color picker refactor

--- components/ui/color-picker/ColorPicker.jsx
+++ components/ui/color-picker/ColorPicker.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import styles from "./ColorPicker.module.css";
 import { CirclePicker } from "react-color";
 
-export const ColorPicker = ({ ...props }) => {
+//TODO: Rewrite this: https://codepen.io/wildbeard/pen/eZZPZr
+// https://stackoverflow.com/questions/17541614/use-images-instead-of-radio-buttons
+//Not accessible
+export const ColorPicker = ({ onChange,colorSwatch,...props }) => {
   return (
     <>
-     
       <h3>Colour Changer</h3>
       <div className={styles.colorPicker}>
-         <CirclePicker colors={["#f44336", "#e91e63", "#9c27b0"]} />
+        <CirclePicker colors={colorSwatch} onChange={onChange} />
       </div>
     </>
   );
